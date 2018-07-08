f821cfdc64facfcd7da22f87a40d33e58111aa78
ENCALADA <encalada@de.ibm.com>
2018-07-08 22:50:53 +0200

    introduce olekukonko/tablewriter pkg, to print table for expenses and bills

--- pkg/ledger/statistic.go
+++ pkg/ledger/statistic.go
@@ -2,21 +2,47 @@ package ledger
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 //GenerateStatsPerMonth will calculate total expenses per current month for both bills and expenses
 func GenerateStatsPerMonth(month Month) {
-	var totalBills float32
-	var totalExpenses float32
-	fmt.Printf("%s %s\n", prettyBlueBold("Retrieving data for user"), prettyGreen(month.User))
-	for _, bills := range month.Bills {
-		totalBills += bills.Amount
-	}
-	for _, expenseValue := range month.Expenses {
-		for _, expensePrice := range expenseValue {
-			totalExpenses += expensePrice.Amount
+	var tableDataBills, tableDataContent [][]string
+	var billsFiltered, expensesFiltered []string
+	var totalBills, totalExpense float32
+
+	for key, value := range month.Expenses {
+		for _, expenses := range value {
+			if expenses.Amount != 0 {
+				expensesFiltered = []string{expenses.Date, expenses.Description, key, (strconv.FormatFloat(float64(expenses.Amount), 'f', 1, 32))}
+				tableDataContent = append(tableDataContent, expensesFiltered)
+				totalExpense += expenses.Amount
+			}
 		}
 	}
-	fmt.Printf("%s %v%s\n", prettyBlueBold("Total amount for Bills:"), prettyGreen(totalBills), prettyGreen("€"))
-	fmt.Printf("%s %v%s\n", prettyBlueBold("Total amount for Expenses:"), prettyGreen(totalExpenses), prettyGreen("€"))
+	for _, value := range month.Bills {
+		billsFiltered = []string{value.Type, value.Description, value.Company, (strconv.FormatFloat(float64(value.Amount), 'f', 1, 32))}
+		tableDataBills = append(tableDataBills, billsFiltered)
+		totalBills += value.Amount
+	}
+
+	fmt.Println("")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Date", "Description", "Location", "Amount"})
+	table.SetFooter([]string{"", "", "Total", strconv.FormatFloat(float64(totalExpense), 'f', 1, 32)})
+	table.SetBorder(true)
+	table.AppendBulk(tableDataContent)
+	table.Render()
+	fmt.Println("")
+	fmt.Println("")
+	tableBills := tablewriter.NewWriter(os.Stdout)
+	tableBills.SetHeader([]string{"Type", "Description", "Location", "Amount"})
+	tableBills.SetFooter([]string{"", "", "Total", strconv.FormatFloat(float64(totalBills), 'f', 1, 32)})
+	tableBills.SetBorder(true)
+	tableBills.AppendBulk(tableDataBills)
+	tableBills.Render()
+	fmt.Println("")
 }
